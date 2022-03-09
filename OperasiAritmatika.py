cef9115193e1d148aa1e2d5a943d1b228699832d
Ahmadnur00 <ahmadnursaidmasfuk@gmail.com>
2022-03-09 17:28:48 +0700

    update

---
+++ OperasiAritmatika.py
@@ -0,0 +1,42 @@
+from re import A
+import numpy as np
+
+#array numpy
+x = np.array([1,2,3,4,5])
+y = np.array([6,7,8,9,10])
+
+#penjumlahan
+hasil_1 = x + y
+print(hasil_1)
+
+#pemgurangan 
+hasil_2 = x - y
+print(hasil_2)
+
+#perkalian
+hasil_3 = x * y
+print(hasil_3)
+
+#pembagian
+hasil_4 = x / y 
+print(hasil_4)
+
+#array multidimensi
+a = np.array(([3,4,5],[1,2,3]))
+b = np.array(([1,2,3],[3,4,5]))
+
+#penjumlahan
+hasil_5 = a + b
+print(hasil_5)
+
+#pemgurangan 
+hasil_6 = a - b
+print(hasil_6)
+
+#perkalian
+hasil_7 = a * b
+print(hasil_7)
+
+#pembagian
+hasil_8 = a / b 
+print(hasil_8)
